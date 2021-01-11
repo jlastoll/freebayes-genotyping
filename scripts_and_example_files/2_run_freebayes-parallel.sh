1374fe6ccb144c9e40ad446b494f9f9fa35ffb4d
jlastoll <52936002+jlastoll@users.noreply.github.com>
2021-01-11 14:17:42 -0500

    Create 2_run_freebayes-parallel.sh

---
+++ scripts_and_example_files/2_run_freebayes-parallel.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#use freebayes to generate a vcf file with all samples in a dataset
+
+#BSUB -q long
+#BSUB -W 6:00
+#BSUB -n 60
+#BSUB -R rusage[mem=1048]
+#BSUB -R "span[hosts=1]"
+#BSUB -e freebayes.err
+#BSUB -oo freebayes.log
+
+
+module load freebayes/1.3.1
+module load picard/2.22.2
+module load samtools/1.9
+
+source /share/pkg/condas/2018-05-11/bin/activate && conda activate freebayes_1.3.1
+#first add readgroup ids to bam files in case they aren't there already with picard- if unsure, just do this step
+#run this script from within the directory containing your sorted, filtered bam files
+
+reference=/path/to/reference/genome.fna
+
+for file in *sortfltr.bam
+do
+date
+echo $file
+sample=$(basename $file _sortfltr.bam )
+#echo $sample
+java -jar /share/pkg/picard/2.17.8/picard.jar AddOrReplaceReadGroups  I= $file O= "$sample"_RG_sort.bam RGID= $sample RGLB= $sample RGPL=illumina RGPU=unit1 RGSM= $sample
+done
+
+#index new bam files
+for file in *RG_sort.bam
+do
+samtools index $file
+done
+
+
+#make new bam file list
+ls *_RG_sort.bam > bamlist.txt
+
+#make sure your targets.bed file is in the directory you
+freebayes-parallel ./targets.bed 60 -f $reference -L ./bamlist.txt --use-best-n-alleles 7 >all_SPECIES_n7.vcf
+
+conda deactivate && conda deactivate
