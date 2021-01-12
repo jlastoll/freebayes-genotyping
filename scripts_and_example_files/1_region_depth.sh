d70aa1ad15a5f88572067af6fd4f8d432ce33b8c
jlastoll <52936002+jlastoll@users.noreply.github.com>
2021-01-12 16:37:03 -0500

    Update 1_region_depth.sh

--- scripts_and_example_files/1_region_depth.sh
+++ scripts_and_example_files/1_region_depth.sh
@@ -14,4 +14,7 @@
 #generate a region list for freebayes from one Bam file breaking apart by depth into 10000 regions
 
 module load samtools/1.9
+module load freebayes/1.3.1 #need freebayes fro the python script called here
+source /share/pkg/condas/2018-05-11/bin/activate && conda activate freebayes_1.3.1
+
 samtools depth  YOUR_BAM_FILE_sortfltr.bam| coverage_to_regions.py PATH/TO/REFERENCE/genomic.fna.fai 10000 >targets.bed
